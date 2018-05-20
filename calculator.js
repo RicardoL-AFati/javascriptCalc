5181845d78275dae00d0e0920ca3e3894134ef3a
RicardoL-AFati <rickydem24@gmail.com>
2018-05-20 13:45:17 -0600

    added AC, CE, and equals functions to calculator

--- calculator.js
+++ calculator.js
@@ -1,10 +1,32 @@
-var calcButtons = {
-    "AC" : "clear",
-    "CE": "clear",
-    "π" : Math.PI.toFixed(3)
+var inputs = [];
+
+var calcFunctions = {
+    AC : function allClear() {
+        inputs = [];
+    },
+    CE: function clearEntry() {
+        inputs.pop();
+    },
+    equals : function equals() {
+        var x = inputs.join("");
+        var result = eval(x);
+        inputs.push("=", result);
+    }
 }
 
-// $("button").on("click", function(){
-//     this.text
-// });
+$(":button").on("click", function(){
+    var pressed = $(this).children().text();
+    if (pressed === "AC") {
+        calcFunctions.AC();
+    } else if (pressed === "CE") {
+        calcFunctions.CE();
+    } else if (pressed == "π") {
+        inputs.push(Math.PI.toFixed(3));
+    } else if (pressed === "=") {
+        calcFunctions.equals();
+    }
+
+
+    console.log(inputs);
+});
 
