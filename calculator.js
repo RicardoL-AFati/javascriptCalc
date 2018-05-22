887f1234b9156a33400f25260be0de63b73ff313
RicardoL-AFati <rickydem24@gmail.com>
2018-05-22 10:28:07 -0600

    Added comments to calculator.js

--- calculator.js
+++ calculator.js
@@ -7,25 +7,31 @@ var totalString;
 //Operators array for validation without the .
 var operators = ["+", "-", "/", "*"];
 
+// Creates string of inputs array -> changes screen span text to that string
 function update(){
     totalString = inputs.join("");
     $("#screen").text(totalString);
 }
-
+// object that contains the three functions that correspond to calculator buttons
 var calcFunctions = {
+    // Clears inputs array and calls update
     AC : function allClear() {
         inputs = [""];
         update();
     },
+    // takes last element off inputs array and calls update 
     CE: function clearEntry() {
         inputs.pop();
-        if (inputs.length == 0) inputs = [""];
         update();
     },
+    // Creates string from inputs, evaluates string as javascript, updates screen span, and updates inputs.
     equals : function equals() {
         totalString = inputs.join("");
+        // result = return from eval(totalString) - rounded to two decimal places 
         var result = Math.round(eval(totalString) * 100) / 100;
+        // setting text of screen span to result
         $("#screen").text(result);
+        // clearing inputs of past operations and setting to result
         inputs = [result];
     }
 }
@@ -39,7 +45,7 @@ function getValue(input) {
     // Check for duplicate operators    
     } else if (operators.includes(inputs[inputs.length - 1]) === false){
         inputs.push(input);
-    // Checks if inputs is a number    
+    // Checks if inputs is a number or "."
     } else if (!isNaN(input) || input === "."){
         inputs.push(input);
     }
@@ -50,18 +56,24 @@ function getValue(input) {
 
 $(":button").on("click", function(){
     if ($(this).children().text() === "AC"){
+        // AC -> calls allClear()
         calcFunctions.AC();
     } else if ($(this).children().text() === "CE") {
+        // CE -> calls clearEntry()
         calcFunctions.CE();
     } else if ($(this).children().text() === "=") {
+        // = -> calls equals()
         calcFunctions.equals();
     } else if ($(this).children().text() === "π") {
+        // Ensures user inputted an operator before clicking PI
         if (operators.includes(inputs[inputs.length - 1]) === false) {
             alert("π must be preceded by an operator: /  *  -  +");
         } else {
+            // getValue with PI rounded to 3 decimal places
             getValue(Math.PI.toFixed(3));
         }
     } else {
+        // All other buttons
         getValue($(this).children().text());
     }
 });
